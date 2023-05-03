68199af4f3db390451d7752217afff4956e5f9a1
AishwaryaGanes <132383076+AishwaryaGanes@users.noreply.github.com>
2023-05-03 14:19:57 +0530

    Add files via upload

---
+++ RestController.java
@@ -0,0 +1,11 @@
+package p;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping(path = "/employees")
+@RestController
+public class RestController {
+
+}
